2cb151b0d958b1eb3c03568c920e1984c5fa69f2
Anton Dachauer <24891375+4d4ch4u32@users.noreply.github.com>
2025-04-08 10:25:42 +0200

    Email sending tests (#9)

--- tests/ApiTests/SendMailTest.php
+++ tests/ApiTests/SendMailTest.php
@@ -6,6 +6,7 @@ namespace App\Tests\ApiTests;
 
 use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 use App\Message\SendEmailMessage;
+use Symfony\Component\Mime\RawMessage;
 use Zenstruck\Messenger\Test\InteractsWithMessenger;
 
 class SendMailTest extends ApiTestCase
@@ -20,5 +21,20 @@ class SendMailTest extends ApiTestCase
 
         $this->transport()->queue()->assertCount(1);
         $this->transport()->queue()->assertContains(SendEmailMessage::class);
+        $this->transport()->processOrFail();
+
+        self::assertEmailCount(1);
+
+        /** @var RawMessage $email */
+        $email = self::getMailerMessage();
+
+        self::assertEmailSubjectContains(
+            $email,
+            'Test-Mail',
+        );
+        self::assertEmailHtmlBodyContains(
+            $email,
+            'Hello world!',
+        );
     }
 }
